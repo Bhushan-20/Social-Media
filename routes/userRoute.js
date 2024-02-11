b99761a1f27865faf685389ad391fb7f9c5532c8
Bhushan-20 <bhushan72845@gmail.com>
2024-02-12 01:19:00 +0530

    Added routes for follow unfollow block unblock

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router();
 
-const {getUser,updateUser,followUser} = require("../controllers/User");
+const {getUser,updateUser,followUser,unFollowUser,blockUser,unBlockUser,BlockedUserList,deleteUser} = require("../controllers/User");
 
 //Get User
 router.get("/:userId",getUser);
@@ -11,6 +11,21 @@ router.put("/update/:userId",updateUser);
 
 //Follow User
 router.post("/follow/:userId",followUser)
+
+//Unfollow User
+router.post("/unfollow/:userId",unFollowUser);
+
+//Block User
+router.post("/block/:userId",blockUser);
+
+//Unblock User
+router.post("/unblock/:userId",unBlockUser)
+
+//Get Blocked User
+router.get("/blocked/:userId",BlockedUserList)
+
+//Delete User
+router.delete("/delete/:userId",deleteUser);
 
 
 
