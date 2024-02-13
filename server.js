bb867bee9014756bf97d55e401def8ba99d593e6
Bhushan-20 <bhushan72845@gmail.com>
2024-02-14 01:39:14 +0530

    Mounted Post Route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser');
 const {errorHandler} = require("./middlewares/error");
-
+const path = require("path")
 
 
 require("dotenv").config();
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 4000;
 //middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads",express.static(path.join(__dirname,"uploads")))
+app.use("/postsimage",express.static(path.join(__dirname,"postsimage")))
 
 //Routes
 const authroute = require("./routes/authroute");
@@ -19,6 +21,10 @@ app.use("/api/v1/auth",authroute);
 const userRoute = require("./routes/userRoute");
 app.use("/api/v2/user",userRoute);
 
+const postRoute = require("./routes/postRoute");
+app.use("/api/v3/post",postRoute);
+
+//Database Connection
 const db = require("./config/database");
 db.connect();
 
