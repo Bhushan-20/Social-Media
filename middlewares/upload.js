7d89897a0259cc058860ba3cbda1d42d88bac6a2
Bhushan-20 <bhushan72845@gmail.com>
2024-02-15 00:14:51 +0530

    Updated upload middleware

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -4,7 +4,6 @@ const path = require("path");
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "uploads/")
-        cb(null,"postsimage")
     },
     filename: function (req, file, cb) {
         const ext = path.extname(file.originalname);
